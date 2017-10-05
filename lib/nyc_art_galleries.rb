3f893da54ac1817d601365af34c366102e50c716
Vicky Lau <vlau.nyc@gmail.com>
2017-10-05 16:56:43 -0400

    added neighborhood_info

--- lib/nyc_art_galleries.rb
+++ lib/nyc_art_galleries.rb
@@ -1,3 +1,5 @@
 # enviorment 
 require_relative "./nyc_art_galleries/version"
+require_relative './nyc_art_galleries/gallery'
+require_relative './nyc_art_galleries/neighborhood_info'
 require_relative './nyc_art_galleries/cli'
