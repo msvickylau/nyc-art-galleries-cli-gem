b305bfc21e3d09bb5ac48a3ddbbc75654177d5ce
Vicky Lau <vlau.nyc@gmail.com>
2017-10-06 18:19:13 -0400

    revised CLI

--- lib/nyc_art_galleries/cli.rb
+++ lib/nyc_art_galleries/cli.rb
@@ -1,5 +1,6 @@
 #CLI Controller
 class NycArtGalleries::CLI
+
 	def call
 		puts "- - - - - - - - - - - - - "
 		puts "Best Art Galleries in NYC:"
@@ -11,7 +12,7 @@ class NycArtGalleries::CLI
 	def list_neighborhood
 		puts "Which neighborhood would you like to visit?"
 		puts <<-DOC.gsub /^\s+/, ""
-			1. Chelsea
+				1. Chelsea
 		    2. Brooklyn
 		    3. Lower East Side
 		    4. Chinatown
@@ -23,46 +24,111 @@ class NycArtGalleries::CLI
 	end
 
 	def gallery_info
-		input = nil
-		while input != "exit"
-			puts "Enter the number of the neighborhood you'd like to view galleries for."
-			puts "   type 'list' to see all neighborhoods again"
-			puts "   type 'info' for more infomation on neighborhoods"
-			puts "   type 'exit' to leave"
-			input = gets.strip.downcase
-			case input 
-			when "1"
-				puts "Galleries in Chelsea"
-			when "2"
-				puts "Galleries in Brooklyn"
-			when "3"
-				puts "Galleries in Lower East Side"	
-			when "4"
-				puts "Galleries in Chinatown"
-			when "5"
-				puts "Galleries in Midtown"
-			when "6"
-				puts "Galleries in Soho, Tribeca, and the West Village"	
-			when "7"
-				puts "Galleries in Uptown"	
-			when "list"
-				list_neighborhood
-			when "info"
-				neighborhood_info
-			when "exit"
-				thanks
-			else
-				puts "Invalid entry, please try again."
-			end				
+		puts "Enter the number of the neighborhood you'd like to view galleries for."
+		# puts " type 'list' to see all neighborhoods again"
+		puts " type 'info' for more information on neighborhoods"
+		# puts " type 'exit' to leave"
+		input = gets.strip.downcase
+		if input == "info"
+			neighborhood_info
+		elsif input == 1-9
+			print_galleries(input)
+		else
+			puts "Invalid entry, please try again."
+		end
+
+		# input = gets.strip.to_i
+
+		puts "What gallery would you like more information on?"
+
+		gallery = @gallery = NycArtGalleries::Gallery.find(input.to_i)
+
+		print_galleries(gallery)
+
+		puts "Would you like to see another gallery? Enter Y or N"
+
+		input = gets.strip.downcase
+		if input == "y"
+			gallery_info
+		else 
+			puts "Thanks! Enjoy your gallery visit!"
+			exit
 		end
 	end
+
+	def print_gallery(neighborhood)
+		puts""
+		puts"- - - - - - Galleries in #{gallery.neighborhood}- - - - - - - "
+		puts""
+		NycArtGalleries::Gallery.all[neighborhood].each do |gallery, index|
+			puts "#{index}. #{gallery.name} - #{gallery.area}"
+		end
+	end
+
+
+	def print_gallery(gallery)
+		puts "- - - - - - #{gallery.name} - #{gallery.area}- - - - - - - "
+		puts ""
+		puts "		Type of Art: 					#{gallery.type_of_art}"
+		puts "		Address:  						#{gallery.address}"
+		puts "		Website: 							#{gallery.website_url}"
+		puts "		Contact: 							#{gallery.contact}"
+		puts "		Opening Hours: 				#{gallery.hours}"
+		puts "- - - - - - - - About the Gallery - - - - - - - -"
+		puts ""
+		puts "#{gallery.about}"
+		puts ""
+	end
+
+
+
+
+
+	# 		if input.to_i > 0
+	# 			puts @gallery[input.to_i-1]
+	# 		elsif input == "list"
+	# 			list_neighborhood
+	# 		elsif input == "info"
+	# 			neighborhood_info
+	# 		elsif input == "exit"
+	# 			puts "Thanks! Enjoy your gallery visit!"
+	# 		else
+	# 			puts "Invalid entry, please try again."
+	# 		end
+	# 	end
+	# end
+					
+
+	# 		case input 
+	# 		when "1"
+	# 			puts "Galleries in Chelsea"
+	# 		when "2"
+	# 			puts "Galleries in Brooklyn"
+	# 		when "3"
+	# 			puts "Galleries in Lower East Side"	
+	# 		when "4"
+	# 			puts "Galleries in Chinatown"
+	# 		when "5"
+	# 			puts "Galleries in Midtown"
+	# 		when "6"
+	# 			puts "Galleries in Soho, Tribeca, and the West Village"	
+	# 		when "7"
+	# 			puts "Galleries in Uptown"	
+	# 		when "list"
+	# 			list_neighborhood
+	# 		when "info"
+	# 			neighborhood_info
+	# 		when "exit"
+	# 			puts "Thanks! Enjoy your gallery visit!"
+	# 		else
+	# 			puts "Invalid entry, please try again."
+	# 		end				
+	# 	end
+	# end
 
 	def neighborhood_info 
 		@info = NycArtGalleries::NeighborhoodInfo.all
 	end
 	
-	def thanks
-		puts "Thanks! Enjoy your gallery visit!"
-	end
-# end
+# end <why does program not run with this?
 
